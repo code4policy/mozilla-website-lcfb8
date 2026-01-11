258ac8ca65a755d60de3ebd446433ddc11fca3fb
LYDIA BROSNAHAN <lydia.brosnahan@gmail.com>
2026-01-11 13:00:52 -0500

    added alert on clicking fox logo

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,3 +2,14 @@
 const myHeading = document.querySelector("h1");
 // Update the text content of the <h1>
 myHeading.textContent = "Hello world!";
+
+ // Define a Function
+ function sayOuch() {
+ 	alert('Ouch! Stop poking me!');
+ }
+ 
+ // Use a CSS selector to identify an element
+ var foxImage = document.querySelector('img');
+ 
+ // Assign the function to the onclick event on that element
+ foxImage.onclick = sayOuch;
